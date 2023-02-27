14c81d81425ca26b1926a7adc27fb957ffa45f7d
rvaquerizo4 <rvaqueri@jaumebalmes.net>
2023-02-27 11:54:21 +0100

    practica 11 acabada

--- M7_raulv_Practica11/routes/web.php
+++ M7_raulv_Practica11/routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+
+Route::get('/ej1/{controladorEjercicio1}', [\App\Http\Controllers\Ej1Controller::class, 'ej1Parametro'])->name('ej1');
+
+Route::get('/ej2/{controladorEjercicio1}', [\App\Http\Controllers\Ej1Controller::class, 'mostrarTexto'])->name('ej2');
+
+Route::get('/modificacion/{controladorEjercicio3}/{practica}/{nombre}/{apellido}', [\App\Http\Controllers\Ej1Controller::class, 'modificacion'])->name('modificacion');
